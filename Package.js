c217378d69696af7aaf5a70bf9921732aacb4761
earnbyad <mahmudul122899@gmail.com>
2026-08-31 08:27:28 +0600

    Initialize Package.js for signaling server project

---
+++ Package.js
@@ -0,0 +1,17 @@
+{
+  "name": "syncme-clone-signaling",
+  "version": "1.0.0",
+  "description": "Signaling server for SyncME-style multi-device remote control",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "ws": "^8.18.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.4"
+  }
+}
